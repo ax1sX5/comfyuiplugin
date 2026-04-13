c6928dae6525deb07337e1dceaa773d2630b0539
ax1sX5 <axisx5@163.com>
2026-04-13 16:59:45 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,8 @@
-# __init__.py
-# 导入 exec 模块以触发路由注册
+# 加载节点
+from . import ExecCommandNode
+
+# 加载接口路由
 from . import exec
 
-# 定义模块公开的接口（可选，增强可读性）
-__all__ = ["exec"]
+# 导出所有（可选，规范写法）
+__all__ = ["ExecCommandNode", "exec"]
